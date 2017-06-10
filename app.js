41f94265e04e394a6f30a30bcd18f0c930985ef5
JJ Conoscenti <glconoscenti@gmail.com>
2017-06-10 18:12:35 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -13,26 +13,26 @@ $(document).ready(function() {
 			choices: ["Hale", "Harvey", "Roosevelt", "Grant"], 
 			questionNumber: 1,
 			correctAnswer: 0,
-		}
+		},
 		{
 			question: "In season 1, what title does Frank ascend to?",
 			choices: ["Speaker of the House", "Vice President", "Majority Whip", "Minority Whip"],
 			questionNumber: 2,
 			correctAnswer: 2,
-		}
+		},
 		{
 			question: "Who is Frank\'s consigliere?",
 			choices: ["Peter Russo", "Remi Danton", "Donald Blythe", "Doug Stamper"],
 			questionNumber: 3,
 			correctAnswer: 3,
-		}
+		},
 		{
 			question: "What does Frank always order at his favorite BBQ restaurant?",
 			choices: ["Steak", "Pulled Pork", "Ribs", "Beans and cornbread"],
 			questionNumber: 4,
 			correctAnswer: 2,
 		}
-	}]
+	]
 
 	//# of correct questions, current question holder, timer
 
@@ -72,16 +72,38 @@ $(document).ready(function() {
 
 	};
 
-
-	// start game
+	// start game with click
 
 	DOM.gameStart.on('click', gameStart);
 
-	// end game
+	
+	// end game with click
+	DOM.endGame.on('click', function() {
+		DOM.transition.hide();
+		DOM.endMessage.html('You finished! Here\'s how you did: ');
+		showResults();
+		DOM.endGame.hide();
+		DOM.results.show();
+		DOM.newGame.show();
+	})
 
-	// verify question
+	// start new game
+	DOM.newGame.on('click', function() {
+		numberRight = 0;
+		numberWrong = 0;
+		numberMissed = 5;
+		currentQuestion = 0;
+		start = 60;
+		finalGrade = 0;
+		DOM.results.hide();
+		DOM.gameStart.show();
+		DOM.nextQuestionButton.show();
+	});
 
-	// next question
+
+
+
+	
 
 
 
